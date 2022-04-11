ad4c1e5b0984c4ddf301d367f9fe3bf162a6fe3a
yassboss <kyh58221022@gmail.com>
2022-04-11 21:47:45 +0900

    ビュー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, :only => [:index, :show]
+  root to: 'users#index'
 end
